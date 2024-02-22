95c4de31d1644bc740be6bece5ed6a2697c2744d
eeescamillar <eeescamillar@libertadores.edu.co>
2024-02-22 09:25:21 -0500

    Desarrollo de los modelos de Chat y message

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express() // inicializacion de la variable que usara la libreria
 const router = express.Router(); // enrutar los servicios web
 const port = 3000; // Escuchar el servidor
 require('dotenv').config() //Obtendremos las variables de entorno
+const socket = require('socket.io')
+const http = require('http').Server(app)
+const io = socket(http)
 
 const DB_URL = process.env.DB_URL || '';
 
@@ -11,8 +14,6 @@ mongoose.connect(DB_URL)
 
 const userRoutes = require('./routes/UserRoutes')
 const houseRoutes = require('./routes/HouseRoutes')
-app.use(express.urlencoded({ extended: true })) // Acceder a la informacion de las urls
-app.use(express.json()) // Analizar informacion en formato JSON
 
 // Metodo [GET, POST, PUT, PATCH, DELETE]
 // NOMBRE DEL SERVICIO [/]      *NUNCA LLEVAN ESPACIOS
@@ -22,12 +23,28 @@ router.get('/', (req, res) => {
   res.send("Hello world")
 })
 
+io.on('connect',(socket) => {
+  console.log('connected')
+//escuchando eventos desde el servidor
+  socket.on('message', (data) => {
+    console.log(data)
 
+    //emitimos mensaje hacia al cliente
+    socket.emit('message-receipt',{"message":"Mensaje recibido en el servidor"})
+  })
+})
+
+app.use(express.urlencoded({ extended: true })) // Acceder a la informacion de las urls
+app.use(express.json()) // Analizar informacion en formato JSON
+app.use((req, res, next) => {
+  res.io = io
+  next
+})
 // ejecutar servidor
 app.use(router);
 app.use('/upload', express.static('upload'))
 app.use('/', userRoutes)
 app.use('/', houseRoutes)
-app.listen(port, () => {
+http.listen(port, () => {
   console.log('listen on ' + port)
 })
--- models/House.js
+++ models/House.js
@@ -8,16 +8,24 @@ const HouseSchema = new mongoose.Schema({
   },
   city: {
     type: String,
-    required: true
+    required: true,
+    validate: {
+      validator: async function(city) {
+        var response = await fetch('https://api.colombia.com/api/v1/City');
+        var cities = await response.json()
+        return cities.some(object => object.name.toUpperCase().includes(city.toUpperCase()));
+      },
+      message: props => '${props.value} no es una Ciudad de Colombia'
+    }
   },
   state: {
     type: String,
     required: true,
     validate: {
       validator: async function(state) {
-        var departments = await fetch('https://api-colombia.com/api/v1/Department');
-        console.log(departments)
-        return true
+        var response = await fetch('https://api-colombia.com/api/v1/Department');
+        var departments = await response.json()       
+        return departments.some(department => department.name.toUpperCase().includes(state.toUpperCase()));
       },
       message: props => '${props.value} no es un departamento de Colombia!!'
     }
